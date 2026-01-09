c4238f9d8138c63d0af1a6a99bbe8fbe58cd93f3
chLi <565353780@qq.com>
2026-01-09 10:27:36 +0800

    create setup bash files

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# pg-sr
+# PGSR
+
+## Setup
+
+```bash
+conda create -n pgsr python=3.10
+conda activate pgsr
+./setup.sh
+```
+
+## Run
+
+```bash
+./train.sh
+```
+
+## Enjoy it~
---
+++ setup.sh
@@ -0,0 +1,18 @@
+cd ..
+git clone --depth 1 https://github.com/facebookresearch/pytorch3d.git
+
+pip install ninja
+
+pip3 install torch torchvision torchaudio \
+  --index-url https://download.pytorch.org/whl/cu128
+
+pip install open3d plyfile opencv-python lpips trimesh
+
+cd pytorch3d
+python setup.py install
+
+cd ../pg-sr/submodules/diff-plane-rasterization
+python setup.py install
+
+cd ../simple-knn
+python setup.py install
